e088d9e9e2a0453b776e518f0fd6ce6ea51e7366
nakamura-yohei <nakamura.yohei@fixer.co.jp>
2019-10-29 19:25:36 +0900

    update: base64に変換するメソッドを追加

--- app/main.py
+++ app/main.py
@@ -1,22 +1,44 @@
 from flask import Flask, jsonify, request
+from io import BytesIO
 import json
+import base64
+from pdf2image import convert_from_path
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'])
 def hello():
     return "route get. Hello!"
+
+
+@app.route("/pdf", methods=['GET'])
+def pdf():
+    images = convert_from_path('sample.pdf')
+    base64_list = []
+    for image in images:
+        buffer = BytesIO()
+        image.save(buffer, format="PNG")
+        base64Img = base64.b64encode(
+            buffer.getvalue()).decode().replace("'", "")
+        base64_list.append(base64Img)
+    result = {
+        "Content-Type": "application/json",
+        "Base64Images": base64_list
+    }
+    return jsonify(result)
+
 
 @app.route('/reply', methods=['POST'])
 def reply():
     data = json.loads(request.data)
     answer = "route post. keyword is %s!\n" % data["keyword"]
     result = {
-      "Content-Type": "application/json",
-      "Answer":{"Text": answer}
+        "Content-Type": "application/json",
+        "Answer": {"Text": answer}
     }
     # return answer
     return jsonify(result)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5001,debug=True)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
