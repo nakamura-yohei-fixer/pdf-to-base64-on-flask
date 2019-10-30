c62aff4d3d7aa756ea5bccc441d3e0faf1d74913
Yohei Nakamura <nakamura.yohei@fixer.co.jp>
2019-10-30 13:01:32 +0900

    feat: base64(pdf) to base64(png)を実装 (#1)
    * add: サンプル用のPDFファイル
    
    * update: popplerのインストール
    
    * update: base64に変換するメソッドを追加
    
    * feat: base64 File => png image => base64 を実装
    
    * remove: サンプル用PDFファイルを削除
    
    * update: ポート番号の変更
    
    * update: dockerfileを移動

--- app/main.py
+++ app/main.py
@@ -1,22 +1,49 @@
 from flask import Flask, jsonify, request
+from io import BytesIO
 import json
+import base64
+from pdf2image import convert_from_bytes
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "route get. Hello!"
 
-@app.route('/reply', methods=['POST'])
-def reply():
-    data = json.loads(request.data)
-    answer = "route post. keyword is %s!\n" % data["keyword"]
+# File => PNG Image => Base64
+# @app.route("/pdf", methods=['POST'])
+# def pdf():
+#     pdf = request.files["file"]
+#     images = convert_from_bytes(pdf.read())
+#     base64_list = []
+#     for image in images:
+#         buffer = BytesIO()
+#         image.save(buffer, format="PNG")
+#         base64Img = base64.b64encode(
+#             buffer.getvalue()).decode().replace("'", "")
+#         base64_list.append(base64Img)
+#     result = {
+#         "Content-Type": "application/json",
+#         "Base64Images": base64_list
+#     }
+#     return jsonify(result)
+
+
+# Base64(PDF) => File => PNG Image => Base64
+@app.route("/pdf", methods=["POST"])
+def pdf():
+    request_data = json.loads(request.data)
+    pdf = base64.b64decode(request_data["pdf"])
+    images = convert_from_bytes(pdf)
+    base64_list = []
+    for image in images:
+        buffer = BytesIO()
+        image.save(buffer, format="PNG")
+        base64Img = base64.b64encode(
+            buffer.getvalue()).decode().replace("'", "")
+        base64_list.append(base64Img)
     result = {
-      "Content-Type": "application/json",
-      "Answer":{"Text": answer}
+        "Content-Type": "application/json",
+        "Base64Images": base64_list
     }
-    # return answer
     return jsonify(result)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5001,debug=True)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
